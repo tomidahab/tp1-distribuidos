daba7d5699a35a278eb43f40958ac42ec82decf8
ivanovic99 <ivanerlich@gmail.com>
2025-04-23 23:04:49 -0300

    Update: boundary retries indefinetly

--- server/boundary/main.py
+++ server/boundary/main.py
@@ -5,25 +5,19 @@ import time
 from Boundary import Boundary
 
 async def main():
-    # Add retry logic for service initialization
-    max_retries = 5
     retry_count = 0
     
-    while retry_count < max_retries:
+    while True:
         try:
             server = Boundary(port=5000)
             await server.run()
-            break  # If run completes successfully, exit the loop
+            break
         except Exception as e:
             retry_count += 1
-            logging.error(f"Error running server: {e}. Retry {retry_count}/{max_retries}")
-            if retry_count < max_retries:
-                wait_time = 2 ** retry_count  # Exponential backoff
-                logging.info(f"Waiting {wait_time} seconds before retrying...")
-                await asyncio.sleep(wait_time)  # Use asyncio.sleep instead of time.sleep
-            else:
-                logging.error("Maximum retries reached. Exiting.")
-                raise
+            logging.error(f"Error running server: {e}. Retry {retry_count}")
+            wait_time = 2 ** retry_count
+            logging.info(f"Waiting {wait_time} seconds before retrying...")
+            await asyncio.sleep(wait_time)
 
 if __name__ == "__main__":
     asyncio.run(main())
